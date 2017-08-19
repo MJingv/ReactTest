998e1ac7da0a42c20abe96962b752537a658cdc1
Jehol <625619954@qq.com>
2017-08-19 20:57:07 +0800

    sth about js test

--- src/index.js
+++ src/index.js
@@ -1,79 +1,11 @@
-//import React from 'react';
-//import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import App from './App';
 
-import React, {Component} from 'react'
-import PropTypes from 'prop-types'
-import ReactDOM from 'react-dom'
-import {createStore} from 'redux'
-import {Provider, connect} from 'react-redux'
+ReactDOM.render(<App />, document.getElementById('root'));
 
-class Counter extends Component {
-  render() {
-    return (
-      <div>
-        <h1>{this.props.value}</h1>
-        <button onClick={this.props.onIncreaseClick}>+</button>
-        <button onClick={this.props.onDecreaseClick}>-</button>
-      </div>
 
-    )
-  }
+for(var i= 0 ;i<10;i++){
+  a = document.getElementById('link')
 }
-
-Counter.propTypes = {
-  value: PropTypes.number.isRequired,
-  onIncreaseClick: PropTypes.func.isRequired
-}
-
-//action
-const increaseAction = {
-  type: 'increase'
-}
-const decreaseAction = {
-  type: 'decrease'
-}
-
-//reducer
-function counter(state = {
-  count: 0
-}, action) {
-  const count = state.count
-  switch (action.type) {
-    case 'increase':
-      return {
-        count: count + 1
-      }
-    case 'decrease':
-      return {
-        count: count - 1
-      }
-
-    default:
-      return state
-
-  }
-
-}
-//store
-const store = createStore(counter)
-
-function mapStateToProps(state) {
-  return {value: state.count}
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    onIncreaseClick: () => dispatch(increaseAction),
-    onDecreaseClick: () => dispatch(decreaseAction)
-  }
-}
-//ReactDOM.render(<App />, document.getElementById('root'));
-
-const App = connect(mapStateToProps, mapDispatchToProps)(Counter)
-
-ReactDOM.render(
-  <Provider store={store}>
-  <App></App>
-</Provider>, document.getElementById('root'))
